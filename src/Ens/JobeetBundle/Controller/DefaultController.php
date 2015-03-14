f9e06ac8bff52c066a80de8081ff8b608d641b32
zak04021 <zakaria.cuet@gmail.com>
2015-03-15 02:24:30 +1100

    commited

--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('EnsJobeetBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('EnsJobeetBundle:Default:index.html.twig', array('name' => $name)); // one comment added
     }
 }
